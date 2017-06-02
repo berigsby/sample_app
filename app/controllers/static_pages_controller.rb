f575d42b82b90de1922f01b087bb9cfe2371fc6e
Ben <ber52305@uga.edu>
2017-06-02 14:51:05 +0000

    clayton testing

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,5 +6,8 @@ class StaticPagesController < ApplicationController
   end
   
   def about
+  #url = 'https://api-eu.hosted.exlibrisgroup.com/almaws/v1/users/{user_id}'.gsub('{user_id}',CGI::escape('811180250'))
+  #headers  = { :params => { CGI::escape('user_id_type') => 'all_unique',CGI::escape('view') => 'full',CGI::escape('expand') => 'none',CGI::escape('apikey') => 'l7xxd74029dc91b54d44a584211c45804704' } }
+  #@response = RestClient::Request.execute :method => 'GET', :url => url , :headers => headers
   end
 end
